13cacd4a35e50cf3d45dac9d9112ef8f79e62c2c
kilgoretrout1985 <zemskyura@gmail.com>
2019-05-07 15:57:44 +0300

    Stuff to release on pypi.

--- setup.py
+++ setup.py
@@ -1,16 +1,41 @@
-from setuptools import setup
+import os
+import setuptools
 
-setup(name='pythonwhois',
-      version='2.4.3',
-      description='Module for retrieving and parsing the WHOIS data for a domain. Supports most domains. No dependencies.',
-      author='Sven Slootweg',
-      author_email='pythonwhois@cryto.net',
-      url='http://cryto.net/pythonwhois',
-      packages=['pythonwhois'],
-      package_dir={"pythonwhois":"pythonwhois"},
-      package_data={"pythonwhois":["*.dat"]},
-      install_requires=['argparse'],
-      provides=['pythonwhois'],
-      scripts=["pwhois"],
-      license="WTFPL"
-     )
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setuptools.setup(
+    name='pythonwhois-alt',
+    version='2.4.4',
+    packages=['pythonwhois'],
+    package_dir={"pythonwhois":"pythonwhois"},
+    package_data={"pythonwhois":["*.dat"]},
+    install_requires=['argparse'],
+    provides=['pythonwhois'],
+    scripts=["pwhois"],
+
+    license="WTFPL",
+    description='Module for retrieving and parsing the WHOIS data for a domain. Supports most domains. No dependencies.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    keywords='whois nic domain',
+
+    author='Yuriy Zemskov',
+    author_email='zemskyura@gmail.com',
+    url='https://github.com/kilgoretrout1985/pythonwhois-alt',
+
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: Name Service (DNS)',
+    ],
+)
